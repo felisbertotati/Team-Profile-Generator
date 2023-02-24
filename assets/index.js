d61f0e82e649113e840f3322dc4fca731a9e4fe2
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-02-24 14:01:49 +0000

    - added const build team -added prompt manager

--- assets/index.js
+++ assets/index.js
@@ -10,6 +10,64 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+// lest build a team
 
+const menuQuestions = [
+  {
+    type: "list",
+    message: "Choose an option",
+    name: `option`,
+    choice: ["Add a Manager", "Add an Engineer", "Add an Intern"],
+  },
+];
+
+const promptManager = () => {
+  return inquirer.prompt([
+    {
+      type: `input`,
+      name: `name`,
+      message: `What is your name? (Required)`,
+      validade: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log(`Please enter your name!`);
+          return false;
+        }
+      },
+    },
+    {
+      type: `input`,
+      name: `id`,
+      message: ` What is your Manager ID? (Required)`,
+      validate: (answer) => {
+        if (answer) {
+          return true;
+        } else {
+          console.log(`Please enter your ID!`);
+          return false;
+        }
+      },
+    },
+    {
+      type: `input`,
+      name: `email`,
+      message: `What is your email adress? (Require)`,
+      validate: (answer) => {
+        if (answer) {
+          return true;
+        } else {
+          console.log("Please provide your Email!");
+          return false;
+        }
+      },
+    },
+    {
+      type: `input`,
+      message: `Enter the office's number: `,
+      name: `OfficeNumber`,
+      validade: (answer) => confirmNumber(answer),
+    },
+  ]);
+};
