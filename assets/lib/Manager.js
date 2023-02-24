6d36c3581163b64aa94676a691a039e55a20c734
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-02-24 13:25:57 +0000

    - added intern class

--- assets/lib/Manager.js
+++ assets/lib/Manager.js
@@ -2,9 +2,9 @@
 const Employee = require("./Employee");
 
 class Manager extends Employee {
-  constructor(name, id, email) {
+  constructor(name, id, email, officeNumber) {
     // super refers to the parent class constructor, and it will call from the `Employee` class with the id, name and email parameters.
-    super(name, id, email, officeNumber);
+    super(name, id, email);
     this.officeNumber = officeNumber;
   }
   getOfficeNumber() {
